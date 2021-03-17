612f4bda5505da3eeb21dbef0061d6dfd940a25a
Noé Fleury <noe.fleury@etu.hesge.ch>
2021-03-17 15:53:49 +0100

    Working, but there is still an issue with one case ("a.*.z")

--- mocks.py
+++ mocks.py
@@ -2,9 +2,9 @@ request_body = {
   "a.*.y.t": "integer",
   "a.*.y.u": "integer",
   # "a.*.z": "object|keys:w,o",
-  # "b": "array",
-  # "b.c": "string",
-  # "b.d": "object",
-  # "b.d.e": "integer|min:5",
-  # "b.d.f": "string"
+  "b": "array",
+  "b.c": "string",
+  "b.d": "object",
+  "b.d.e": "integer|min:5",
+  "b.d.f": "string"
 }
--- utils.py
+++ utils.py
@@ -16,30 +16,45 @@ def add_tree_part(json, type, key):
         return json["items"]
 
     elif type == TYPE_OBJECT:
+        if not 'properties' in json:
+            json["properties"] = {}
         if not key in json["properties"]:
             json["properties"][key] = {"type": "object", "validators": ["object"], "properties": {}}
         return json["properties"][key]
 
 
 def add_leaf(json, key, validators):
-        if not key in json["properties"]:
-            json["properties"][key] = {"type": "leaf", "validators": [validators]}
+        if validators == "array":
+            add_tree_part(json, TYPE_OBJECT, key)
+        elif not key in json["properties"]:
+            json["properties"][key] = {"type": "leaf", "validators": [ v for v in validators.split('|')] }
 
 def expand_validator(json):
     output = {}
     for keys in json:
         key = keys.split('.')
-        latest = None
+        latest = output
         for i in range(len(key)):
-            if i == 0:
+            if i == 0 and not i == len(key)-1:
                 if key[i] not in output:
                     output[key[i]] = {}
+                latest = output[key[i]]
             elif key[i] == '*':
                 latest = add_tree_part(output[key[i-1]], TYPE_ARRAY, None)
             else:
-                if i < len(key)-1:
-                    latest = add_tree_part(latest, TYPE_OBJECT, key[i])
+                if len(key) == 1 and json[keys] == "array":
+                    tmp = {}
+                    add_tree_part(tmp, TYPE_OBJECT, key[i])
+                    output[key[i]] = tmp['properties'][key[i]]
+                elif i == len(key)-1:
+                    if json[keys] != "object":
+                        add_leaf(latest, key[i], json[keys])
+                    elif json[keys] == "object":
+                        latest = add_tree_part(latest, TYPE_OBJECT, key[i])
                 else:
-                    add_leaf(latest, key[i], json[keys])
+                    if not key[i] in latest:
+                        add_tree_part(latest, TYPE_OBJECT, key[i])
+                    latest = latest['properties'][key[i]]
+
 
     return output
